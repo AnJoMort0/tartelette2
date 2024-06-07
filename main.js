fd4145de33989ea00f1dfd6f52b744357a96bfcf
AnJoMort0 <andrejsfonseca@gmail.com>
2024-06-07 16:19:27 +0200

    Rebalancing

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const PIE_SCALE = 0.32;
 const CLICK_JUMP = 1.05;
 let nextIngr = 0;
 let score = 0;
-let time = 300;
+let time = 90;
 
 loadRoot('assets/');
 loadSprite('empty',"empty.png");
@@ -161,7 +161,7 @@ scene("game", () => {
         pos(W/2, H/2 + 270),
         z(2),
     ])
-    loop(1, () => {
+    loop(0.75, () => {
         time--;
         for (let i = 0; i < randi(0,4); i++) {
             let randIngr = choose(ingr)
@@ -317,7 +317,7 @@ scene("gameOver", () => {
         "button",
     ]);
     onClick("replayButton", () => {
-        time = 300;
+        time = 90;
         go("game");
     });
     // Menu button
